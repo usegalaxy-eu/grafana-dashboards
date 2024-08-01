054646938a94eccffd7aa648e35152773730654f
José Manuel Domínguez <dominguj@informatik.uni-freiburg.de>
2024-08-01 10:44:45 +0200

    Export Grafana dashboards from stats.usegalaxy.eu
    Export dashboards using a GitHub Actions workflow, which replaces `export.sh`.  The latter reads the dashboards from the database and is designed to run periodically as a cron job on the stats host. The former reads the dashboards from the Grafana API and runs periodically on GitHub actions.
    
    The motivation behind exporting the dashboards via the Grafana API is that it will guarantee that private dashboards do not get to this Git repository, as the workflow has the same permissions as any anonymous user.

---
+++ .github/actions/action-grafana-dashboards-export/action.py
@@ -0,0 +1,91 @@
+"""Retrieve publicly available dashboards from a Grafana instance.
+
+Fetch publicly available dashboards from a Grafana instance via API calls and
+save them to JSON files.
+
+- `sys.argv[1]`: URL of the Grafana instance to retrieve Dashboards from.
+- `sys.argv[2]`: Directory to save dashboard JSON files to.
+
+Dashboards are retrieved and saved concurrently using `aiohttp` and `aiofiles`.
+"""
+
+import asyncio
+import json
+import sys
+from typing import AsyncIterator
+
+import aiofiles
+import aiohttp
+
+
+AIOHTTP_MAX_CONCURRENT_REQUESTS: int = 50
+
+
+async def list_dashboards(
+    grafana_url: str,
+    session: aiohttp.ClientSession,
+) -> AsyncIterator[str]:
+    """Retrieve list of dashboards from search endpoint.
+
+    References:
+      - [1] https://grafana.com/docs/grafana/latest/developers/http_api/folder_dashboard_search/  # noqa
+    """
+    limit = AIOHTTP_MAX_CONCURRENT_REQUESTS - 1
+    page = 1
+    more_pages = True
+
+    while more_pages:
+        async with session.get(
+            f"{grafana_url}/api/search?limit={limit}?page={page}?query=&",
+            headers={"Content-Type": "application/json"},
+        ) as response:
+            response_json = await response.json()
+            for dashboard in (
+                item for item in response_json if item.get("type") == "dash-db"
+            ):
+                yield dashboard
+
+        if len(response_json) < limit:
+            more_pages = False
+        else:
+            page += 1
+
+
+async def save_dashboard_json(
+    uid: str, grafana_url: str, path: str, session: aiohttp.ClientSession
+) -> str:
+    """Retrieve a dashboard by uid and save it to a JSON file."""
+    async with session.get(
+        f"{grafana_url}/api/dashboards/uid/{uid}",
+        headers={"Content-Type": "application/json"},
+    ) as response:
+        dashboard_json = await response.json()
+        metadata = dashboard_json["dashboard"]
+        async with aiofiles.open(
+            f"{path}/{metadata.get('title') or metadata.get('uid')}.json", "w"
+        ) as file:
+            json_string = json.dumps(dashboard_json, indent=2)
+            await file.write(json_string)
+
+
+async def main(grafana_url: str, path: str):
+    """Retrieve and save publicly available dashboards from a Grafana instance."""
+    connector = aiohttp.TCPConnector(limit=AIOHTTP_MAX_CONCURRENT_REQUESTS)
+    async with aiohttp.ClientSession(connector=connector) as session:
+        tasks = [
+            asyncio.create_task(
+                save_dashboard_json(
+                    dashboard["uid"],
+                    grafana_url,
+                    path,
+                    session,
+                )
+            )
+            async for dashboard in list_dashboards(grafana_url, session)
+        ]
+        await asyncio.gather(*tasks)
+
+
+if __name__ == "__main__":
+    grafana_url, path = sys.argv[1:]
+    asyncio.run(main(grafana_url, path))
